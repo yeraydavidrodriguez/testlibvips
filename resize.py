d84c7f290066eba55d614b376811afac4922205d
Yeray Rodriguez <yeray.rodriguez@edataconsulting.es>
2021-09-27 14:47:10 +0100

    Workflow modifications

--- resize.py
+++ resize.py
@@ -2,4 +2,4 @@ import pyvips
 
 image = pyvips.Image.new_from_file('raw/test.tif', access='sequential')
 result = image.resize(0.5, kernel='lanczos2')
-result.write_to_file("out.tif")
+result.write_to_file("raw/test-gold.tif")
